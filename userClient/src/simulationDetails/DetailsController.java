b56782d6a448aea08c8b843d7d52fc03e45b9c53
Moran Pol <104639084+moranpol@users.noreply.github.com>
2023-09-30 19:32:44 +0300

    start of screen 1 and 2 in the user

---
+++ userClient/src/simulationDetails/DetailsController.java
@@ -0,0 +1,25 @@
+package simulationDetails;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.layout.Pane;
+
+public class DetailsController {
+
+    @FXML
+    private Pane detailsPane;
+
+    @FXML
+    private Button showWorldButton;
+
+    @FXML
+    private ComboBox<String> worldCBox;
+
+    @FXML
+    void showWorldButtonClicked(ActionEvent event) {
+
+    }
+
+}
